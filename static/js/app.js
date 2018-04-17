40a5de36c5b3434cd34a70c53e11c683e2e99c28
lijie-02@newrank.cn <lijie-02@newrank.cn>
2018-04-17 21:20:11 +0800

    11

--- static/js/app.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  // 此方法是$(document).ready(function(){})的简写
-  var $topEle = $('.top');
-  var $contentEle = $('.content');
-  var $footerEle = $('.footer');
-  $topEle.html('top');
-  $contentEle.html('content');
-  $footerEle.html('footer');
-})
